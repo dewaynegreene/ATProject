8c1f1dfbf5cc8d7e2268bb514c810eb05fa3b720
JaNae <janaestrickland701@gmail.com>
2020-04-10 22:24:51 -0400

    edited main file, added convert.cpp

---
+++ convert.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string convert (string);
+
+string convert (string grammar)
+{
+    //use islower() and isupper() functions
+    //get grammar line by line
+    string line = "", push_grammar = "";
+
+    int i = 0; //i is declared outside of for loop scope so the index picks up where one line left off
+    char x = 'l', check = *grammar.begin();
+    char end = --*grammar.end();
+
+    //deals with grammar line by line
+    while(check != end)
+    {
+        for(i; i < grammar.size(); i++)
+        {
+            x = grammar.at(i);
+
+            if(x == '\n') break;
+
+            line += x;
+            check++;
+        }
+
+        //deal with line
+    }
+
+    return push_grammar;
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <string>
+#include "convert.cpp"
 
 using namespace std;
 
 int main()
 {
-    cout << "Enter your Context-Free Grammar";
+    string input = "", hold = "";
 
+    cout << "Enter your Context-Free Grammar in GNF (enter 'x' to stop): ";
+    cout << endl << endl;
+    
+    for(;;)
+    {
+        getline(cin, hold);
+        if(hold == "x") break;
+        input += hold + '\n';
+    }
+
+    cout << "\nGrammar:\n" << input;
+
+    convert(input);
+
+    return EXIT_SUCCESS;
 }
