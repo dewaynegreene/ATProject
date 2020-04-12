afa8978d22f37daf02e6ca5f537e55d4b68767e3
JaNae <janaestrickland701@gmail.com>
2020-04-12 15:38:56 -0400

    changed convert function, still needs to work on writing PDA productions

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
+#include <cstring>
 #include <string>
 #include <map>
 #include <vector>
-#include <cstring>
 
 using namespace std;
 
 map<string, vector<string>> rules; //fix code to reflect this new vector element
 vector<string> terminals;
 
-string convert();//string);
+void convert();//string);
 
 int main()
 {
     string input = "", hold = "", convert_input = "", terminal = "", prod = "";
+    vector<string> prod_vec;
 
     cout << "Enter your Context-Free Grammar in GNF (enter 'x' to stop): ";
     cout << endl << endl;
@@ -43,72 +44,71 @@ int main()
                 {
                     
                     handle += p;
-                    
+
                     if((p == '|') || (p == '\n'))
                     {
                         //push back handle
-                        
+                        prod_vec.push_back(handle);
                         handle = "";
                         continue;
                     }
                 }
 
-                rules.insert(pair<string, string> (terminal, prod)); //insert prod into second's vector
-
+                rules.insert(pair<string, vector<string>> (terminal, prod_vec)); //insert prod into second's vector
+                
                 // for(pair<string, string> p: rules)
                 // cout << p.first << " " << p.second;
             }
     }
 
-    cout << "\nGrammar:\n" << input;
-
-    convert_input = convert();//input);
+    cout << "\nGrammar:\n" << input << endl;
+    
+    convert();
 
     cout << "\nBye\n";
 
     return EXIT_SUCCESS;
 }
 
-string convert()//string grammar)
+void convert()//string grammar)
 {
     //use islower() and isupper() functions
     //PDA will have 3 states, q0 with transition to q1 with marking (lambda, z, Sz), q1 with loop to itself
     //with bunch of markings based on the grammar, and qf - final state with transition q1 to qf
     //with marking (lambda, z, lambda).
 
-    string push_grammar = "";
-    vector<string> input_symbols = "";
+    vector<string> input_symbols;
 
-    string g_line = "";
-    g_line = "d(q0, lambda, z) = { (q1, Sz) }" + '\n';
-    push_grammar += g_line;
+    cout << "d(q0, lambda, z) = { (q1, Sz) }" << endl;
 
     //loop through every production line
     for(int i = 0; i < rules.size(); i++)
     {
         auto t = terminals.at(i); //current terminal
-        pair<string, string> p = rules[t];
-        input_symbols = p.second;
+        pair<string, vector<string>> p {t, rules[t]}; //must be a string
         
-        //convert input_symbols to c string to access characters
-        char new_input_symbols[input_symbols.size() + 1];
-        strcpy(new_input_symbols, input_symbols.c_str());
-
-        //loop through the right side of arrow, character by character
-        for(int j = 0; j < input_symbols.size(); j++)
+        for(int x = 0; x < p.second.size(); x++)
         {
-            //aAB
-            if(islower(new_input_symbols.at(j))) g_line = "d(q1, " + new_input_symbols.at(j) + ", " + t + ") = { (q1,";
-                else g_line += new_input_symbols.at(j);
+            //loop through the right side of arrow, character by character
+            for(int j = 0; j < input_symbols.size(); j++)
+            {
+                //aAB
+                string s = p.second.at(x); //one of the rules
+                auto c = s[j]; //one rule at index j
+                cout << "\n";
+
+                // if(islower(c))
+                // {   
+                //    //d(q1, a, S) = { (q1,
+                //     cout << "d(q1, " + c;
+                //     cout << ", " + t + ") = { (q1,"; 
+                // } else cout << c;
+            }
         }
-
-        g_line += ") }\n"; 
-
-        push_grammar += g_line;
+ 
+        // cout << ") }\n";
     }
 
-    g_line = "d(qf, lambda, z) = { (qz, lambda) }" + '\n';
-    push_grammar += g_line;
+    cout << "d(qf, lambda, z) = { (qz, lambda) }";
 
-    return push_grammar;
 }
