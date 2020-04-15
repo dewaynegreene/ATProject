bddfcd82fcb376e696b97b8774774b476d54156e
JaNae <janaestrickland701@gmail.com>
2020-04-15 03:16:12 -0400

    through struggles and hard times, we have fixed main function, almost finished ocnvert function, still need to verify PDA output

--- main.cpp
+++ main.cpp
@@ -1,65 +1,66 @@
 #include <iostream>
 #include <cstring>
 #include <string>
-#include <map>
 #include <vector>
 
 using namespace std;
 
-map<string, vector<string>> rules; //fix code to reflect this new vector element
-vector<string> terminals;
+void convert();
 
-void convert();//string);
+vector<vector<string>> grammar;
+vector<string> line;
 
 int main()
 {
     string input = "", hold = "", convert_input = "", terminal = "", prod = "";
-    vector<string> prod_vec;
 
     cout << "Enter your Context-Free Grammar in GNF (enter 'x' to stop): ";
     cout << endl << endl;
     
     for(;;)
     {
-        getline(cin, hold);
+        getline(cin, hold); //S->ab
+
         if(hold == "x") break;
             else
             {
-                input += hold + '\n';
+                input = hold + '\n';
                 terminal = *hold.begin();
-                terminals.push_back(terminal);
-                // cout << "terminal: " << terminal << endl;
+                line.push_back(terminal);
                 
                 //load production variable
-                auto iter = hold.begin() + 3;
-                while(iter != hold.end()) // handle case: | adD | adf 
+                auto iter = input.begin() + 3;
+                prod = "";
+                while(iter != input.end())
                 {
-                    prod += *iter;
-                    // cout << "Prod: " << prod << endl;
+                    prod += *iter; //loaded with everything to the right of the arrow
                     ++iter;
                 }
-
+                
                 string handle = "";
+                
                 for(auto p: prod) //loops through whole production to the right of the arrow
                 {
+                    if(isalpha(p)) handle += p;
                     
-                    handle += p;
-
-                    if((p == '|') || (p == '\n'))
+                    if((p =='\n') || (p == '|'))
                     {
-                        //push back handle
-                        prod_vec.push_back(handle);
+                        line.push_back(handle);
                         handle = "";
-                        continue;
                     }
+                    
                 }
-
-                rules.insert(pair<string, vector<string>> (terminal, prod_vec)); //insert prod into second's vector
-                
-                // for(pair<string, string> p: rules)
-                // cout << p.first << " " << p.second;
             }
+
+        grammar.push_back(line);
+        line.clear();
     }
+
+    // for(auto g : grammar)
+    // {
+    //     for(auto l : g)
+    //     cout << l << " ";
+    // }
 
     cout << "\nGrammar:\n" << input << endl;
     
@@ -77,36 +78,35 @@ void convert()//string grammar)
     //with bunch of markings based on the grammar, and qf - final state with transition q1 to qf
     //with marking (lambda, z, lambda).
 
-    vector<string> input_symbols;
+    /*
+    (q1, a, a) = {q1, lambda)
+    (q1, b, b) = {q1, lambda)
+    (q1, lambda, S) = {q1, aAB)
+    (q1, lambda, B) = {q1, b)
+    (q1, lambda, A) = {q1, aAB)
+    */
 
+    int x = 0;
     cout << "d(q0, lambda, z) = { (q1, Sz) }" << endl;
 
-    //loop through every production line
-    for(int i = 0; i < rules.size(); i++)
+    //loop one production line at a time
+    for(int i = 0; i < grammar.size(); i++) //rules = everything to the right of the arrow
     {
-        auto t = terminals.at(i); //current terminal
-        pair<string, vector<string>> p {t, rules[t]}; //must be a string
-        
-        for(int x = 0; x < p.second.size(); x++)
+        for(int j = 0; j < grammar[i].size() - 1; j++)
         {
-            //loop through the right side of arrow, character by character
-            for(int j = 0; j < input_symbols.size(); j++)
+            
+            cout << "d(q1, " << grammar[i][1].at(x) << ", " << grammar[i][0] << ") = {(q1, ";
+            for(int k = 1; k < grammar[i][1].size(); k++)
             {
-                //aAB
-                string s = p.second.at(x); //one of the rules
-                auto c = s[j]; //one rule at index j
-                cout << "\n";
-
-                // if(islower(c))
-                // {   
-                //    //d(q1, a, S) = { (q1,
-                //     cout << "d(q1, " + c;
-                //     cout << ", " + t + ") = { (q1,"; 
-                // } else cout << c;
+                cout << grammar[i][1].at(k);
             }
-        }
- 
-        // cout << ") }\n";
+            // grammar[0] = s aBBBB
+            // grammar [1] = a cD
+            cout << ")} " << endl;
+            x++;
+        } 
+        x = 0;
+        cout << endl;
     }
 
     cout << "d(qf, lambda, z) = { (qz, lambda) }";
